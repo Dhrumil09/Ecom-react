154411f343012d81177a88d946acc3b65eee3d77
bacancy-Dhrumil <dhrumil.shah@bacancy.com>
2023-07-22 11:17:34 +0530

    initial commit

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer">
-      <div className="footer-col-1">
+      {/* <div className="footer-col-1">
         <strong>
           <span>
             Developed by
@@ -21,22 +21,22 @@ const Footer = () => {
             <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
           </span>
         </strong>
-      </div>
-      <div className="footer-col-2">
+      </div> */}
+      {/* <div className="footer-col-2">
         <img alt="Footer logo" className="footer-logo" src={logo} />
         <h5>
           &copy;&nbsp;
           {new Date().getFullYear()}
         </h5>
-      </div>
-      <div className="footer-col-3">
+      </div> */}
+      {/* <div className="footer-col-3">
         <strong>
           <span>
             Fork this project &nbsp;
             <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
           </span>
         </strong>
-      </div>
+      </div> */}
     </footer>
   );
 };
--- src/views/admin/products/index.jsx
+++ src/views/admin/products/index.jsx
@@ -10,7 +10,7 @@ import { ProductsNavbar } from '../components';
 import ProductsTable from '../components/ProductsTable';
 
 const Products = () => {
-  useDocumentTitle('Product List | Salinaka Admin');
+  useDocumentTitle('Product List | Shoes Shop');
   useScrollTop();
 
   const store = useSelector((state) => ({
